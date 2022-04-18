494708c1b4cd38b58c8d955bda5bdd5041ff76dd
abdul <abdul.tanveer@gmail.com>
2022-04-18 14:28:10 +0530

    add method/function

--- Javabasics_harman/src/basics/Demo.java
+++ Javabasics_harman/src/basics/Demo.java
@@ -1,12 +1,16 @@
 package basics;
 
 public class Demo {
-	
+//---------------------------------------------------	
 	public static void main(String[] args) {
-		for(int i=0; i<11 ; i++) {
-			System.out.println("hello world--"+i);
-
-		}
+		int sum;
+		sum = aparnaAdd(10,20);
+		System.out.println("the sum is --"+sum);
+	}
+//-------------------------------------------------------------
+	static int aparnaAdd(int fno, int sno) {
+		int result =  fno + sno;
+		return result;
 	}
 
 }
