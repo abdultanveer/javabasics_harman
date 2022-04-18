07b5bc335ff60e449e2c798f22aa5ed45688dff1
abdul <abdul.tanveer@gmail.com>
2022-04-18 14:41:20 +0530

    student box

--- Javabasics_harman/src/basics/Demo.java
+++ Javabasics_harman/src/basics/Demo.java
@@ -6,6 +6,9 @@ public class Demo {
 		int sum;
 		sum = aparnaAdd(10,20);
 		System.out.println("the sum is --"+sum);
+		Student collegeStudent = new Student(); //new keyword -- create a box in the ram
+		collegeStudent.name = "abdul";
+		System.out.println("Name of the student is--"+collegeStudent.name);
 	}
 //-------------------------------------------------------------
 	static int aparnaAdd(int fno, int sno) {
---
+++ Javabasics_harman/src/basics/Student.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Student {
+	//i'll create 5 partitions
+	String name;
+	int age;   //4 bytes -- int
+	boolean isMale;  //1byte
+	String address;
+
+}
