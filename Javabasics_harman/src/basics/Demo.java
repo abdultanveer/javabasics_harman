bfdc0abd1f75fdd2611b5d92ed9630e6afa85a98
abdul <abdul.tanveer@gmail.com>
2022-04-18 12:54:16 +0530

    for loop

--- Javabasics_harman/src/basics/Demo.java
+++ Javabasics_harman/src/basics/Demo.java
@@ -3,7 +3,10 @@ package basics;
 public class Demo {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		for(int i=0; i<11 ; i++) {
+			System.out.println("hello world--"+i);
+
+		}
 	}
 
 }
