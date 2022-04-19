ac3dfae0fc12acb0680f0018b6ee14b4455b70d1
abdul <abdul.tanveer@gmail.com>
2022-04-19 11:04:58 +0530

    constructors

--- Javabasics_harman/src/basics/Demo.java
+++ Javabasics_harman/src/basics/Demo.java
@@ -5,16 +5,25 @@ public class Demo {
 	public static void main(String[] args) {
 		int sum;
 		sum = aparnaAdd(10,20);   //10 & 20 are arguments
-		System.out.println("the sum is --"+sum);
-		Student collegeStudent = new Student(); //new keyword -- create a box in the ram
-		collegeStudent.askQuestion();
-		collegeStudent.name = "abdul";
-		System.out.println("Name of the student is--"+collegeStudent.name);
+		//System.out.println("the sum is --"+sum);
+		Student collegeStudent = new Student(123,"ansari"); //new keyword -- create a box in the ram
+		
+		
+		System.out.println("student name is---"+ collegeStudent.name);
+		System.out.println("student age is---"+ collegeStudent.age);
+
+		Student javaStudent = new Student();
+		javaStudent.name = "abdul";
+		/*
+		 * collegeStudent.askQuestion(); collegeStudent.name = "abdul";
+		 * System.out.println("Name of the student is--"+collegeStudent.name);
+		 */
 	}
 //-------------------------------------------------------------
 	 private static int aparnaAdd(int fno, int sno) { //fno and sno are params
 		int result =  fno + sno;
 		return result;
 	}
+	//--------------------------
 
 }
--- Javabasics_harman/src/basics/Student.java
+++ Javabasics_harman/src/basics/Student.java
@@ -1,16 +1,32 @@
 package basics;
 
 public class Student {
-	//i'll create 5 partitions
+	//i'll create 5 partitions  -- instance variable
 	String name;
 	int age;   //4 bytes -- int
 	boolean isMale;  //1byte
 	String address;
+	//-------------------------------
+	public Student(){
+		System.out.println("default constructor--shreyas");
+	}
 	
-	
+	//----------------------------------
+	public Student(String sname) {
+		System.out.println("aditya constructor");
+		name = sname;
+	}
+	//---------------------------------
+	public Student(int sage,String sname) {
+		name = sname;
+		age = sage;
+	}
+	//-----------------------
 	void askQuestion() {
 		System.out.println("asking java questions");
 	}
+	//--------------------------
 	void writeExam() {}
+	//--------------------------
 
 }
