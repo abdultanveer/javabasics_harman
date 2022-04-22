b68ed7f5b6d370f58979930335c4c0ca773dd0d3
abdul <abdul.tanveer@gmail.com>
2022-04-22 09:54:40 +0530

    jdbc- where clause

--- Javabasics_harman/src/basics/jdbc/DemoJdbc.java
+++ Javabasics_harman/src/basics/jdbc/DemoJdbc.java
@@ -5,11 +5,16 @@ import java.sql.*;
 
 public class DemoJdbc {
 	public static void main(String[] args) throws ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		try {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/harman_db","root","");
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from persons");
+			//Statement statement = connection.createStatement();
+			String name = "abdul";
+			PreparedStatement statement = connection.prepareStatement("select * from persons where FirstName = ?");    
+			statement.setString(1, name);    
+			ResultSet resultSet = statement.executeQuery();
+			
+			//ResultSet resultSet = statement.executeQuery("select  * from persons where FirstName ="+name);
 			resultSet.next();
 			int personId = resultSet.getInt("PersonID");
 			String fname = resultSet.getString("FirstName");
